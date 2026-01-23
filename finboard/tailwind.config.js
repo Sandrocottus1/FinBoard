fc9abe324f55b5ec71d2a01dbcebf1813866f788
Aryan Yadav <74095206+Sandrocottus1@users.noreply.github.com>
2026-01-24 01:23:33 +0530

    tailwind.consfig/ts ->js

--- finboard/tailwind.config.js
+++ finboard/tailwind.config.js
@@ -1,7 +1,6 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-  darkMode: 'class', 
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class", // 👈 This is the line that matters
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -12,4 +11,3 @@ const config: Config = {
   },
   plugins: [],
 };
-export default config;
